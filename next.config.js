e993176ae02b21504d140ec67c6c6276f766700d
unknown <favoursnazy>
2022-04-15 20:49:46 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  env : {
+    "NEXT_PUBLIC_APPID" : "ihrIGyufjD8qi3BAYecjdYDeZCZw76SuOfM2cmAZ",
+    "NEXT_PUBLIC_SERVER_URL" : "https://kcy4tamreets.usemoralis.com:2053/server"
+  }
+}
